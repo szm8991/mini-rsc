cee43342f646d1b0ea5cc676876be325ee1efd71
songzheming <songzheming@163.com>
2023-08-15 14:39:19 +0800

    update: support react FC

--- server.js
+++ server.js
@@ -5,30 +5,39 @@ import { escapeHtml } from "./escape.js";
 createServer(async (req, res) => {
   const author = "Jae Doe";
   const postContent = await readFile("./posts/hello-world.txt", "utf8");
-  sendHTML(
-    res,
-    <html>
-    <head>
-      <title>My blog</title>
-    </head>
-    <body>
-      <nav>
-        <a href="/">Home</a>
-        <hr />
-      </nav>
-      <article>{postContent}</article>
-      <footer>
-        <hr />
-        <p>
-          <i>
-            (c) {author} {new Date().getFullYear()}
-          </i>
-        </p>
-      </footer>
-    </body>
-  </html> 
-  );
+  sendHTML(res, <BlogPostPage author={author} postContent={postContent} />);
 }).listen(8080);
+
+function BlogPostPage({ postContent, author }) {
+  return (
+    <html>
+      <head>
+        <title>My blog</title>
+      </head>
+      <body>
+        <nav>
+          <a href="/">Home</a>
+          <hr />
+        </nav>
+        <article>{postContent}</article>
+        <Footer author={author} />
+      </body>
+    </html>
+  );
+}
+
+function Footer({ author }) {
+  return (
+    <footer>
+      <hr />
+      <p>
+        <i>
+          (c) {author} {new Date().getFullYear()}
+        </i>
+      </p>
+    </footer>
+  );
+}
 
 function sendHTML(res, jsx) {
   const html = renderJSXToHTML(jsx);
@@ -45,19 +54,26 @@ function renderJSXToHTML(jsx) {
     return jsx.map((child) => renderJSXToHTML(child)).join("");
   } else if (typeof jsx === "object") {
     if (jsx.$$typeof === Symbol.for("react.element")) {
-      let html = "<" + jsx.type;
-      for (const propName in jsx.props) {
-        if (jsx.props.hasOwnProperty(propName) && propName !== "children") {
-          html += " ";
-          html += propName;
-          html += "=";
-          html += escapeHtml(jsx.props[propName]);
+      if (typeof jsx.type === "string") {
+        let html = "<" + jsx.type;
+        for (const propName in jsx.props) {
+          if (jsx.props.hasOwnProperty(propName) && propName !== "children") {
+            html += " ";
+            html += propName;
+            html += "=";
+            html += escapeHtml(jsx.props[propName]);
+          }
         }
-      }
-      html += ">";
-      html += renderJSXToHTML(jsx.props.children);
-      html += "</" + jsx.type + ">";
-      return html;
+        html += ">";
+        html += renderJSXToHTML(jsx.props.children);
+        html += "</" + jsx.type + ">";
+        return html;
+      } else if (typeof jsx.type === "function") {
+        const Component = jsx.type;
+        const props = jsx.props;
+        const returnedJsx = Component(props);
+        return renderJSXToHTML(returnedJsx);
+      } else throw new Error("Not implemented.");
     } else throw new Error("Cannot render an object.");
   } else throw new Error("Not implemented.");
 }
